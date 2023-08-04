1f5dcee8495acf635882abf08dadc3473e892d1b
tamiresns <tamiresmn@live.com>
2023-08-04 16:11:27 -0300

    finalizando o footer - css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Menu from './components/Menu';
 import Footer from './components/Footer';
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,23 +7,31 @@ const Footer = () => {
     return (
       <footer className="footer">
         <h1>Contato</h1>
-        <section className="social-icons">
-            <a href="URL_DO_FACEBOOK" target="_blank" rel="noopener noreferrer">
-                <FaFacebook className='icon'/>
-            </a>
-            <a href="URL_DO_INSTAGRAM" target="_blank" rel="noopener noreferrer">
-                <FaInstagram className='icon'/>
-            </a>
-            <a href="URL_DO_WHATSAPP" target="_blank" rel="noopener noreferrer">
-                <FaWhatsapp className='icon'/>
-            </a>
-            <a href="URL_DO_YOUTUBE" target="_blank" rel="noopener noreferrer">
-                <FaYoutube className='icon'/>
-            </a>
-        </section>
-        <p>
+        <ul className="social-icons">
+            <li>
+                <a href="URL_DO_FACEBOOK" target="_blank" rel="noopener noreferrer">
+                    <FaFacebook className='icon' alt='Ícone do Facebook'/>
+                </a>
+            </li>
+            <li>
+                <a href="URL_DO_INSTAGRAM" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram className='icon' alt='Ícone do Instagram'/>
+                </a>
+            </li>
+            <li>
+                <a href="URL_DO_WHATSAPP" target="_blank" rel="noopener noreferrer">
+                    <FaWhatsapp className='icon' alt='Ícone do Whatsapp'/>
+                </a>
+            </li>
+           <li>
+                <a href="URL_DO_YOUTUBE" target="_blank" rel="noopener noreferrer">
+                    <FaYoutube className='icon' alt='Ícone do Youtube'/>
+                </a>
+           </li>         
+        </ul>
+        <p className='author'>
             © {new Date().getFullYear()} Desenvolvido por{' '}
-            <a href="https://github.com/tamiresns" target="_blank" rel="noopener noreferrer">Tamires Matos</a>.
+            <a href="https://github.com/tamiresns" target="_blank" rel="noopener noreferrer" aria-label="Perfil do GitHub da desenvolvedora Tamires Matos">Tamires Matos</a>.
         </p>      
       </footer>
     );
